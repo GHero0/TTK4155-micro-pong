0936eb23a7464d705ea1f54d3f3b7b69ac0d013f
GHero0 <george13vladut@gmail.com>
2025-09-15 20:05:58 +0200

    chore(struct-modif): Add global & constants files

---
+++ src/global.h
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+/* 
+ * global.h
+ * This header file is included in every .c file of the project.
+ * It is intended to contain extern declarations of global variables and shared definitions.
+ * Actual global variable definitions should be in a globaldec.h
+ */
+#include "constants.h"
+
+
+#endif
